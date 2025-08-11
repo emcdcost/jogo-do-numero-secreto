8e8acf445d8e98b8f56717014a4b7146a3945fcd
emcdcost <emcdcost92@gmail.com>
2025-08-11 19:58:26 -0300

    deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,11 +12,11 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
